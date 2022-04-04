ade049629f98795202e67cf249b52202a1c6771c
ElJohnnie <johnfollmer1995@gmail.com>
2022-04-04 18:38:42 -0300

    first commit

---
+++ my_site/authenticate/forms.py
@@ -0,0 +1,37 @@
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from django.contrib.auth.models import User
+from django import forms
+
+class EditProfileForm(UserChangeForm):
+	
+	password = forms.CharField(label="", widget=forms.TextInput(attrs={'type':'hidden'}))
+	class Meta:
+		model = User
+		#excludes private information from User
+		fields = ('username', 'first_name', 'last_name', 'email','password',)
+class SignUpForm(UserCreationForm):
+	email = forms.EmailField(label="", widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'Email'}), )
+	first_name = forms.CharField(label="", max_length=100, widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'Primeiro nome'}))
+	last_name = forms.CharField(label="", max_length=100, widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'Último nome'}))
+	
+	
+	class Meta:
+		model = User
+		fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2',)
+
+	def __init__(self, *args, **kwargs):
+	    super(SignUpForm, self).__init__(*args, **kwargs)
+	    self.fields['username'].widget.attrs['class'] = 'form-control'
+	    self.fields['username'].widget.attrs['placeholder'] = 'Usuário'
+	    self.fields['username'].label = ''
+	    self.fields['username'].help_text = '<span class="form-text text-muted"><small>Requerido 150 caracteres ou menos. Apenas letras, dígitos e @/./+/-/_.</small></span>'
+
+	    self.fields['password1'].widget.attrs['class'] = 'form-control'
+	    self.fields['password1'].widget.attrs['placeholder'] = 'Senha'
+	    self.fields['password1'].label = ''
+	    self.fields['password1'].help_text = '<ul class="form-text text-muted small"><li>Sua senha não pode ser muito parecida com suas outras informações pessoais.</li><li>Sua senha deve conter pelo menos 8 caracteres.</li><li>Sua senha não pode ser uma senha comumente usada.</li><li>Sua senha não pode ser totalmente numérica.</li></ul>'
+
+	    self.fields['password2'].widget.attrs['class'] = 'form-control'
+	    self.fields['password2'].widget.attrs['placeholder'] = 'Confirme sua senha'
+	    self.fields['password2'].label = ''
+	    self.fields['password2'].help_text = '<span class="form-text text-muted"><small>Digite a mesma senha de antes, para verificação.</small></span>'
