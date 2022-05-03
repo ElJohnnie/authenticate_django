cde22341041aa20fc8d3045084ce6ceec4734009
Daniel Barboza <danibarbozad@gmail.com>
2022-05-03 20:39:24 -0300

    patient model creation

--- my_site/authenticate/models.py
+++ my_site/authenticate/models.py
@@ -2,6 +2,71 @@ from django.db import models
 
 # Create your models here.
 
+class Patient(models.Model):
+    first_name = models.CharField(
+        blank=False,
+        null=False,
+        max_length=100
+    )
+    last_name = models.CharField(
+        blank=False,
+        null=False,
+        max_length=100
+    )
+    date_of_birth = models.DateField(
+        blank=False,
+        null=False
+    )
+    email = models.CharField(
+        blank=False,
+        null=False,
+        max_length=100
+    )
+    phonenumber = models.CharField(
+        blank=False,
+        null=False,
+        max_length=100
+    )
+    landline_phonenumber = models.CharField(
+        max_length=100,
+        blank=True,
+        null=True
+    )    
+    zipcode = models.CharField(
+        blank=False,
+        null=False,
+        max_length=100
+    )
+    neighborhood = models.CharField(
+        blank=False,
+        null=False,
+        max_length=100
+    )
+    city = models.CharField(
+        blank=False,
+        null=False,
+        max_length=100
+    )
+    address = models.CharField(
+        blank=False,
+        null=False,
+        max_length=100
+    )
+    complement = models.CharField(
+        max_length=100,
+        blank=True,
+        null=True
+    )
+    state = models.CharField(
+        blank=False,
+        null=False,
+        max_length=100
+    )
+    objects = models.Manager()
+    
+
+
+
 class Vacines(models.Model):
     date = models.DateField(
         blank=False,
